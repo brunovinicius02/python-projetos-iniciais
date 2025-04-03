7064e9ff914ce6fbdbd0487cd9c5681b7a28ee04
brunovinicius02 <brunovbarbosaa@gmail.com>
2025-04-03 20:00:44 -0300

    Gerador de senhas fortes e aleatórias

---
+++ gerador_de_senhas.py
@@ -0,0 +1,8 @@
+import random
+import string
+
+def gerar_senha(tamanho=12):
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(random.choice(caracteres) for _ in range(tamanho))
+
+print("Senha gerada:", gerar_senha())
